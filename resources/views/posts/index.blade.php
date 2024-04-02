de3a701c92344fcf19b0e3cbf063437c41c6a647
小林廉太朗 <kobaren@kobayashirentarounoMacBook-Air.local>
2024-04-02 13:51:37 +0900

    課題3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,17 @@
     <title>投稿一覧</title>
 </head>
 <body>
-    <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{$post->title}}</td>
+                <td>{{$post->content}}</td>
+            </tr>
+        @endforeach
+    </table>
 </body>
 </html>
